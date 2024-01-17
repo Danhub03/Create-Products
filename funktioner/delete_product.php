9218daae6c0114b9da2bc886b804c56d3e0dbdbd
Danhub03 <118169550+Danhub03@users.noreply.github.com>
2024-01-17 11:20:08 +0100

    Update delete_product.php

--- funktioner/delete_product.php
+++ funktioner/delete_product.php
@@ -34,7 +34,6 @@ table {
             width: 60%;
             border-collapse: collapse;
             overflow: scroll;
-            /* margin:auto; */
         }
 
         table img {
@@ -46,7 +45,6 @@ table {
             padding: 15px;
             text-align: center;
             border: 2px solid #ddd;
-            /* vertical-align:center; */
         }
 
         th {
@@ -58,18 +56,18 @@ table {
 <body>
 
 <?php
-// Här kopplar det upp mot databasen som jag skapade enligt instruktionerna som jag skrev i indexen.
-// Dessa variabler är definierade för att kunna ansluta till en databas (MySql-databas) då det är servernamnen, användarnamn,
-// lösenord och databasnamn ($dbname).
+// Here it connects to the database that I created according to the instructions that I wrote in the indexes.
+// These variables are defined to be able to connect to a database (MySql database) as they are the server names, usernames,
+// password and database name ($dbname).
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "crud_app";
 
-// Med hjälp av de definierade variablerna skapar jag här en ny anslutning till den skapade MySQL-databasen. 
+// Using the defined variables, here I create a new connection to the created MySQL database.
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Här skickar den ett felmeddelande om anslutningen misslyckas.
+// Here it sends an error message if the connection fails.
 if ($conn->connect_error) {
     die("Anslutning misslyckades: " . $conn->connect_error);
 }
@@ -79,12 +77,12 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $sql = "DELETE FROM products WHERE id = $id";
 
-    // Här körs sql-frågan då om frågan lyckas kommer if-satsens kod att köras annars om det misslyckas skriver det ut ett fel-
-    // meddelande.
+    // Here the sql query is executed then if the query succeeds the code of the if statement will be executed otherwise if it fails it will print an error-
+    // message.
     if ($conn->query($sql) === TRUE) {
-         header("Location: add_product.php"); // Här omdirigerar den webbläsaren tillbaka till sidan add_product.php efter att
-        // produkten har blivit borttagen.
-         exit(); // Här avslutar den PHP-koden vilket gör att webbsidan avslutas.
+         header("Location: add_product.php"); // Here it redirects the browser back to the add_product.php page after that
+        // the product has been removed.
+         exit(); // Here it ends the PHP code which causes the web page to end.
     } else {
         echo "Fel: " . $sql . "<br>" . $conn->error;
     }
